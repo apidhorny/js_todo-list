324150efd521e08e7ad7ac2706f7470c55a01d30
Andriy <andriy.pidhorny@gmail.com>
2021-11-04 23:45:22 +0200

    ui and create li

---
+++ js/app.js
@@ -0,0 +1,72 @@
+const tasks = [
+    {
+        _id: '5d2ca9e2e03d40b326596aa7',
+        completed: true,
+        body: 'Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n',
+        title: 'Eu ea incididunt sunt consectetur fugiat non.',
+    },
+    {
+        _id: '5d2ca9e29c8a94095c1288e0',
+        completed: false,
+        body: 'Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n',
+        title: 'Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.',
+    },
+    {
+        _id: '5d2ca9e2e03d40b3232496aa7',
+        completed: true,
+        body: 'Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n',
+        title: 'Eu ea incididunt sunt consectetur fugiat non.',
+    },
+    {
+        _id: '5d2ca9e29c8a94095564788e0',
+        completed: false,
+        body: 'Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n',
+        title: 'Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.',
+    },
+];
+
+(function (arrOfTasks) {
+    const objOfTasks = arrOfTasks.reduce((acc, task) => {
+        acc[task._id] = task;
+        return acc;
+    });
+
+    renderAllTasks(objOfTasks);
+
+    function renderAllTasks(tasksList) {
+        if (!tasksList) {
+            console.error('Error');
+            return;
+        }
+
+        const fragment = document.createDocumentFragment();
+        Object.values(tasksList).forEach((task) => {
+            const li = listItemTemplate(task);
+            fragment.appendChild(li);
+        });
+    }
+
+    function listItemTemplate({ _id, title, body } = {}) {
+        const li = document.createElement('li');
+        li.classList.add('task-item');
+
+        const h5 = document.createElement('h5');
+        h5.textContent = title;
+        h5.classList.add('task-body');
+
+        const p = document.createElement('p');
+        p.textContent = body;
+        p.classList.add('task-title');
+
+        const btn = document.createElement('button');
+        btn.textContent = 'Delete task';
+        btn.classList.add('btn', 'btn-delete-task');
+        console.log(li);
+
+        li.appendChild(h5);
+        li.appendChild(p);
+        li.appendChild(btn);
+
+        return li;
+    }
+})(tasks);
